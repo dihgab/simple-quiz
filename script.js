ec6686305826c7075e8cfb4dc9fe941c37b311d6
Diego Gabriel <diegogabriel598@gmail.com>
2023-11-11 22:42:55 -0400

    press and hold

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ longPressButton.addEventListener('mousedown', () => {
     pressTimer = setTimeout(() => {
         // Ação que acontece após o long-press
         message.textContent = "achei!";
-    }, 1000); // 1000 milissegundos = 1 segundo
+    }, 3000); // 1000 milissegundos = 1 segundo
+
+    // Adiciona a classe "active" ao botão durante o pressionamento do mouse
+    longPressButton.classList.add('active');
 });
 
 longPressButton.addEventListener('mouseup', () => {
     clearTimeout(pressTimer);
     message.textContent = "";
+    // Remove a classe "active" quando o botão for liberado
+    longPressButton.classList.remove('active');
 });
